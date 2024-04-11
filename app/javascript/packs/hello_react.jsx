1f17d7d3670563ed56b75df58b78c888e9392aad
Zhenya Telyukov <telyukov@gmail.com>
2024-04-11 15:53:48 +0300

    updated hello-react

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,26 +1,9 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// Add element to the existing HTML content
+document.body.innerHTML += '<div id="hi"></div>';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+// Render your React component instead
+const root = createRoot(document.getElementById('hi'));
+root.render(<h2>Hi, React 18!</h2>);
